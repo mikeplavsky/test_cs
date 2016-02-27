376caf9aa1d9c106037172a38ecd5c92aaefad6d
Ubuntu <ubuntu@ip-10-180-38-138.ec2.internal>
2016-02-27 11:16:48 +0000

    serializing

---
+++ Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.IO;
+
+using System.Runtime.Serialization;
+using System.Runtime.Serialization.Json;
+
+[DataContract]
+internal class Person {
+
+  [DataMember]
+  internal string name;
+
+  [DataMember]
+  internal int age;
+
+}
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        var p = new Person {name = "Jim", age = 20 };
+
+        var sr = new DataContractJsonSerializer(typeof(Person));
+        var mem = new MemoryStream();
+
+        sr.WriteObject(mem, p);
+        mem.Position = 0;
+
+        var r = new StreamReader(mem);
+        var res = r.ReadToEnd();
+
+        Console.WriteLine(res);
+    }
+}
